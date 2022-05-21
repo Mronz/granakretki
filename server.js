91af36a0e4350768339c47d33fd6fb96510c896b
Aleksander <o.latas@onet.pl>
2022-05-21 12:55:06 +0200

    severjnode

--- server.js
+++ server.js
@@ -1,1 +1,11 @@
 //olek pierdol sie
+var express = require("express")
+var app = express()
+const PORT = 3000;
+app.use(express.json())
+app.use(express.text())
+
+
+app.listen(PORT, function () {
+    console.log("start serwera na porcie " + PORT)
+})
